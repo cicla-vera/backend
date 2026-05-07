ee314cdc9b2ad75b99203eb93f4e35a557077a6d
kaeffea <kaeffea@gmail.com>
2026-05-07 06:17:20 -0300

    feat: add cycle prediction service with next period, ovulation and fertile window
    Closes BED-28

--- src/cycles/cycles.module.ts
+++ src/cycles/cycles.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { CyclesService } from './cycles.service';
 import { CyclesController } from './cycles.controller';
 import { AuthModule } from '../auth/auth.module';
+import { CyclesPredictionService } from './cycles.prediction.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [CyclesController],
-  providers: [CyclesService],
-  exports: [CyclesService],
+  providers: [CyclesService, CyclesPredictionService],
+  exports: [CyclesService, CyclesPredictionService],
 })
 export class CyclesModule {}
